e3d947eb54a98f02ebaeee96f85719f242fb14a2
asa1997 <arunsureshampadath@gmail.com>
2020-10-07 23:36:29 +0530

    Completed front end for index.html

--- src/phonenumber_verification_service/phonenumber_verification/models.py
+++ src/phonenumber_verification_service/phonenumber_verification/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Verification(models.Model):
+    phone_number = PhoneNumberField(max_length=10, null=False, blank=False)
+    connection_id = models.UUIDField()
+    invite_url = models.URLField(max_length=2000)
+
+class SessionState(models.Model):
+    connection_id = models.UUIDField()
+    state = models.TextField()
--- src/phonenumber_verification_service/phonenumber_verification/views.py
+++ src/phonenumber_verification_service/phonenumber_verification/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import PhonenumberForm
+
 
 def index(request):
-    return render(request,'index.html')
+    return render(request,'index.html',{"form": PhonenumberForm()})
